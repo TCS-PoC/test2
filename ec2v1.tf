7a6a537cb55953efe39d96c0063ec28e2483a8f1
shalabh-a <37323672+shalabh-a@users.noreply.github.com>
2018-03-20 21:36:15 +0530

    Update ec2v1.tf

--- ec2v1.tf
+++ ec2v1.tf
@@ -34,3 +34,16 @@ resource "aws_instance" "instance3" {
     Name = "hostTerraform3"
   }
 }
+
+#Configure the AWS EC2resource
+resource "aws_instance" "instance4" {
+  ami = "ami-26ebbc5c"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "key1"
+  security_groups = ["sg-d4b425a0"]
+  subnet_id = "subnet-2b059476" 
+  tags {
+    Name = "hostTerraform4"
+  }
+}
